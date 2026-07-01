620fbdbd5e6218572127200259d59a33bc38af75
Denis Novikov <voothi@gmail.com>
2026-07-01 03:43:25 +0200

    20260701034325

--- kardenwort_desk.py
+++ kardenwort_desk.py
@@ -939,7 +939,7 @@ def run_render_flow(text, language, zid, text_mode, config, resolved_paths, zoom
             f'<td class="{lemma_class}" data-col="WordSource">{lemma_val}</td>'
             f'<td class="{trans_class}" data-col="WordDestination">{trans_val}</td>'
             f'<td>{ipa_val}</td>'
-            f'<td>{morph_val}</td>'
+            f'<td><div class="scrollable-cell">{morph_val}</div></td>'
             f'</tr>'
         )
     table_rows_html = "\n".join(table_rows)
@@ -1104,9 +1104,19 @@ def run_render_flow(text, language, zid, text_mode, config, resolved_paths, zoom
     width: auto;
     padding-right: 12px;
   }
+  .scrollable-cell {
+    width: 100%;
+    box-sizing: border-box;
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+  }
+  .scrollable-cell::-webkit-scrollbar {
+    display: none; /* Chrome, Safari and Opera */
+  }
   /* When the window is NOT maximized (normally sized) */
   body:not(.maximized) {
     max-width: 100vw;
+    overflow-x: hidden;
   }
   body:not(.maximized) .container {
     display: block;
@@ -1115,6 +1125,17 @@ def run_render_flow(text, language, zid, text_mode, config, resolved_paths, zoom
   }
   body:not(.maximized) .section {
     max-width: 100%;
+  }
+  body:not(.maximized) .scrollable-cell {
+    overflow-x: auto;
+    white-space: nowrap;
+    max-width: 250px;
+  }
+  /* When maximized */
+  body.maximized .scrollable-cell {
+    overflow-x: visible;
+    white-space: normal;
+    max-width: none;
   }
   th {
     text-align: left;
