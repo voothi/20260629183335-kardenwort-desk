df1d02220f136c8614d6bebac50f297bb7b36fef
Denis Novikov <voothi@gmail.com>
2026-07-09 02:56:37 +0200

    20260709025636

--- kardenwort_desk.py
+++ kardenwort_desk.py
@@ -2604,7 +2604,7 @@ def run_render_flow(text, language, zid, text_mode, config, resolved_paths, zoom
                                     var div = tds[2].querySelector('.scrollable-cell');
                                     var val = rowData.trans || "";
                                     var oldVal = div ? div.textContent : (tds[2].classList.contains('editing') ? null : tds[2].textContent);
-                                    var hasSkeleton = (div ? div.innerHTML : tds[2].innerHTML).indexOf('skeleton-loader') > -1;
+                                    var hasSkeleton = (div || tds[2]).querySelector('.skeleton-loader') !== null;
                                     if (oldVal !== val || hasSkeleton) {
                                         if (div) div.textContent = val;
                                         else if (!tds[2].classList.contains('editing')) tds[2].textContent = val;
@@ -2615,7 +2615,7 @@ def run_render_flow(text, language, zid, text_mode, config, resolved_paths, zoom
                                     var div = tds[3].querySelector('.scrollable-cell');
                                     var val = rowData.ipa || "";
                                     var oldVal = div ? div.textContent : (tds[3].classList.contains('editing') ? null : tds[3].textContent);
-                                    var hasSkeleton = (div ? div.innerHTML : tds[3].innerHTML).indexOf('skeleton-loader') > -1;
+                                    var hasSkeleton = (div || tds[3]).querySelector('.skeleton-loader') !== null;
                                     if (oldVal !== val || hasSkeleton) {
                                         if (div) div.textContent = val;
                                         else if (!tds[3].classList.contains('editing')) tds[3].textContent = val;
