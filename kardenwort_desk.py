3af455b453673a9b966a4ec134a7a83a09927085
Denis Novikov <voothi@gmail.com>
2026-08-13 14:35:00 +0200

    20260813143459

--- kardenwort_desk.py
+++ kardenwort_desk.py
@@ -3552,8 +3552,6 @@ html, body {{
     llm_filled = is_tsv_llm_filled(headers, data_rows, mapping)
     
     main_text_provider = config.get(SEC_PIPELINE, 'text_base_provider', fallback=config.get(SEC_PIPELINE, 'lemma_base_provider', fallback='google'))
-    lemmas_provider = config.get(SEC_PIPELINE, 'lemma_reprocess_provider', fallback='intellifiller')
-    
     role_fields = get_role_fields(mapping, headers)
         
     col_highlighted = headers.index(role_fields['selected']) if 'selected' in role_fields and role_fields['selected'] in headers else -1
